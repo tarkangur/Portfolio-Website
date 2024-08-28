079133540355a4413e5069123b8eac82c7bae304
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-08-29 00:39:58 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, PasswordField, EmailField, TextAreaField
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 
@@ -23,3 +23,10 @@ class AddProject(FlaskForm):
     url = StringField("Project URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Project Content", validators=[DataRequired()])
     submit = SubmitField("Submit Project")
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send Message')
